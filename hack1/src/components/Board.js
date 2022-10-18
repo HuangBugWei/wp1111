ee745c0d4553f1fbfde0992dfaa5cc2b02c7a358
HuangBugWei <b06501042@g.ntu.edu.tw>
2022-10-18 10:10:19 +0800

    todo 2 finish

--- hack1/src/components/Board.js
+++ hack1/src/components/Board.js
@@ -16,6 +16,8 @@ const Board = ({ turn, guesses, curGuess }) => {
         <div className="Board-container">
             {/* TODO 2-2: show 6 rows (map function is recommended) and defined row's key.
                 Hint: Use `CurRow` instead of `Row` when you are passing `curGuess` into it. */}
+            { guesses.map((guess, idx) => (idx === turn) ? <CurRow id={"row_" + idx} key={"row_" + idx} curGuess={curGuess} /> : <Row id={"row_" + idx} key={"row_" + idx} guess={guess}/>)}
+            
             
         </div>
     )
