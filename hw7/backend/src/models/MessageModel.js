734899b6a56e8a9a883e29e63f57eda6abebb59b
HuangBugWei <b06501042@g.ntu.edu.tw>
2022-11-28 17:37:37 +0800

    update frontend & backend

---
+++ hw7/backend/src/models/MessageModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema
+
+/******* Message Schema *******/
+const MessageSchema = new Schema({
+    chatBox: { type: mongoose.Types.ObjectId, ref: 'ChatBox' },
+    sender: { type: mongoose.Types.ObjectId, ref: 'User' },
+    body: { type: String, required: [true, 'Body field is required.'] },
+});
+const MessageModel = mongoose.model('Message', MessageSchema);
+
+export default MessageModel
