821ba963f8c6f81f570a0f4350a7e1954f6e3048
HuangBugWei <b06501042@g.ntu.edu.tw>
2022-12-20 12:03:59 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -7,12 +7,19 @@ import Category from './Category';
 
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
-  // const {
-  //   loading, error, data: itemsData, subscribeToMore,
-  // } = useQuery(GET_ITEMS_QUERY);
+  const {
+    loading, error, data: itemsData, subscribeToMore,
+  } = useQuery(GET_ITEMS_QUERY);
+  const { items } = itemsData;
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return (<p>Error :(</p>);
+  }
   // const items = itemsData.items;
-  const { data } = useQuery(GET_ITEMS_QUERY);
-  const { items } = data;
+  // const { data } = useQuery(GET_ITEMS_QUERY);
+  // const { items } = data;
   // TODO 2.2 End
 
 
