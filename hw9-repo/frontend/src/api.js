ecfa9488cc0b0aa02d303f6febd307f45c239f2e
HuangBugWei <b06501042@g.ntu.edu.tw>
2022-12-26 14:45:58 +0800

    fix frontend axios

--- hw9-repo/frontend/src/api.js
+++ hw9-repo/frontend/src/api.js
@@ -9,9 +9,9 @@ const API_ROOT =
     ? "/"
     : "http://localhost:4000/";
 
-export const instance = axios.create({ baseURL: API_ROOT });
+const instance = axios.create({ baseURL: API_ROOT });
 
-// export default instance;
+export default instance;
 
 // instance.get('/hi').then((data) => console.log(data));
 // yarn add -D @babel/cli @babel/core @babel/node @babel/preset-env @babel/plugin-proposal-class-properties @babel/plugin-proposal-object-rest-spread @babel/plugin-transform-arrow-functions dotenv-defaults mongoose
